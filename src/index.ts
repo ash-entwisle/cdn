b761b0caf3225d62dc6f11cfd7c036f9d47d92b8
Ash Entwisle <ash.entwisle@protonmail.com>
2024-02-09 11:35:24 +0000

    moved to astro
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from "hono";
-const app = new Hono();
-
-// app.use("/*", serveStatic({ root: "./public" }));
-app.get("/", (c) => { return c.redirect("https://proot.dev")});
-
-export default app;
